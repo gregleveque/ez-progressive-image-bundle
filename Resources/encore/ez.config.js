4408ee06c0cf9db9b8cf2a263b28d89573e20170
Grégory LÉVÊQUE <greg.leveque.gie@gmail.com>
2020-02-06 11:08:50 +0100

    [UPDATE] placeholder height & lazyload

--- Resources/encore/ez.config.js
+++ Resources/encore/ez.config.js
@@ -1,6 +1,13 @@
 const path = require('path')
 
-module.exports = Encore => Encore.addStyleEntry(
-  'ez-progressive-image-css',
-  path.resolve(__dirname, '../public/scss/images.scss')
-)
+module.exports = Encore => {
+  Encore.addStyleEntry(
+    'ez-progressive-image-css',
+    path.resolve(__dirname, '../public/scss/images.scss')
+  )
+
+  Encore.addEntry(
+    'ez-progressive-image-js',
+    path.resolve(__dirname, '../public/js/lazysizes.min.js')
+  )
+}
