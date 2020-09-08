f2b0735d36b6ebb64957bea2856d064df9df53ac
Grégory LÉVÊQUE <greg.leveque.gie@gmail.com>
2020-09-08 15:42:30 +0200

    [FIX] sf5 config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ez_progressive_image');
+        $treeBuilder = new TreeBuilder('ez_progressive_image');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
